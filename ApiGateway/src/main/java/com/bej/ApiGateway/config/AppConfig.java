11b182e61b138b0397156a9f3b7d880c3af277f5
Ketki Keni <sketki220@gmail.com>
2023-02-21 06:39:50 +0530

    added ApiGateway

---
+++ ApiGateway/src/main/java/com/bej/ApiGateway/config/AppConfig.java
@@ -0,0 +1,28 @@
+/*
+ * Author : Ketki Keni
+ * Date : 14-02-2023
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.bej.ApiGateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route(p->p.path("/api/v1/**")
+//                        .uri("http://localhost:8082/"))
+                        .uri("lb://customer-authentication-service"))
+                .route(p->p.path("/api/v2/**")
+//                        .uri("http://localhost:8083/"))
+                        .uri("lb://customer-pizza-service"))
+                .build();
+    }
+}
